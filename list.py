c033669deceaf840bbf8126f5a8ad51e826b6a52
yash dollarawar <yashdollarwar11@gmail.com>
2024-05-20 19:11:56 +0530

    list_task
    this is the list task.

---
+++ list.py
@@ -0,0 +1,24 @@
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1)
+print(list2)
+print(list3)
+list4 = ["abc", 34, True, 40, "male"]
+print(list4)
+print(list4)
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+# note the double round-brackets
+thislist = list(("apple", "banana", "cherry")) 
+print(thislist)
+
